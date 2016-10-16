6c10f62b72e0c238a2f2250b8a0518f05935ab58
Robert Hardy <robbert@gmail.com>
2016-10-16 18:43:53 +0000

    Example of how to use Bootstrap.

--- app/Main.elm
+++ app/Main.elm
@@ -1,4 +1,24 @@
-import Html
+import Html exposing (..)
+import Html.Attributes exposing (..)
 
 
-main = Html.text "Hello World"
+stylesheet =
+    let
+        tag = "link"
+        attrs =
+            [ attribute "rel"       "stylesheet"
+            , attribute "property"  "stylesheet"
+            , attribute "href"      "//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css"
+            ]
+        children = []
+    in
+        node tag attrs children
+
+
+main =
+    let
+        inner = div [id "inner", class "container"] [h1 [class "text-center"] [text "hello flash of unstyled content"]]
+        hero  = div [id "hero", class "jumbotron"] [inner]
+
+    in
+        div [id "outer"] [stylesheet, hero]
