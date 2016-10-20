2815e17624b314a49ad8886460a5e09d368b342d
Robert Hardy <robbert@gmail.com>
2016-10-20 04:59:20 +0000

    All works.

--- app/Main.elm
+++ app/Main.elm
@@ -8,23 +8,26 @@ listToShow = [ "hello", "world" ]
 
 main =
   let
-    inner =
-      ul [class "list-group"] (List.map (\s -> li [] [ text s ]) listToShow)
+    theList =
+      ul [class "list-group"] (List.map (\s -> li [class "list-group-item"] [ text s ]) listToShow)
   in
     div [class "container"]
-      [ panelHeading "A list"
-      , div [] [ inner ]
+      [ panel "A list" theList
       ]
 
 panelHeading title =
   div
-    [class "panel panel-default"]
+    [class "panel-heading"]
     [
       div
-        [class "panel-heading"]
-        [
-          div
-            [class "panel-title"]
-            [ h1 [] [ text title ] ]
-        ]
+        [class "panel-title"]
+        [ h1 [] [ text title ] ]
+    ]
+
+
+panel title content =
+  div
+    [class "panel panel-default"]
+    [ panelHeading title
+    , content
     ]
