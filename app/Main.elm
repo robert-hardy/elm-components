f26635c339370aec5c178ff1ad8e2af516f8b50e
Robert Hardy <robbert@gmail.com>
2016-10-20 04:31:40 +0000

    Modify structure of html a little.

--- app/Main.elm
+++ app/Main.elm
@@ -4,8 +4,8 @@ import Html.Attributes exposing (..)
 
 main =
     let
-        inner = div [id "inner", class "container"] [h1 [class "text-center"] [text "hello flash of unstyled content"]]
+        inner = div [id "inner"] [h1 [class "text-center"] [text "hello flash of unstyled content"]]
         hero  = div [id "hero", class "jumbotron"] [inner]
 
     in
-        div [id "outer"] [hero]
+        div [id "outer", class "container"] [inner]
