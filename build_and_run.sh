f3d8fac0d43cdb9b07b6d564e63108a98e6a2d23
Robert Hardy <robbert@gmail.com>
2016-11-10 06:48:00 +0000

    Pass appname into the flask template.

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,2 +1,2 @@
 APPNAME=${1:-ToDoList}
-elm-make app/"{APPNAME}"/App.elm --output app/static/my-app.js && python run.py
+elm-make app/"$APPNAME"/App.elm --output app/static/my-app.js && python run.py APPNAME
--- run.py
+++ run.py
@@ -2,15 +2,20 @@ from flask import (
     jsonify,
     render_template
 )
+import sys
 
 
 from app.flasky import app
+
+
+appname = sys.argv[1]
 
 
 @app.route('/')
 def homepage():
     return render_template(
         'index.html'
+        appname=appname
     )
 
 
