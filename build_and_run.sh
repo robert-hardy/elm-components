4f2f876aabeca5bcf5977d828864828b1aaa88b9
Robert Hardy <robbert@gmail.com>
2016-11-10 06:59:24 +0000

    Fix fullscreen.

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,2 +1,2 @@
 APPNAME=${1:-ToDoList}
-elm-make app/"$APPNAME"/App.elm --output app/static/my-app.js && python run.py $APPNAME
+elm-make app/"$APPNAME"/App.elm --output app/static/app.js && python run.py $APPNAME
