6ba2dcdce6b5b6fc3bd38150b05408d56e903d41
Robert Hardy <robbert@gmail.com>
2016-11-20 08:57:45 +0000

    App view.

--- app/frontend/AppMain/View.elm
+++ app/frontend/AppMain/View.elm
@@ -13,45 +13,12 @@ root model =
 
         Just w ->
             let
-                panelHeader =
-                    div
-                        [ class "panel-heading" ]
-                        [ div
-                            [ class "panel-title" ]
-                            [ h1 [] [ text "Word echo" ] ]
-                        ]
-
-                panelBody =
-                    div
-                        [ class "panel-body" ]
-                        [ text "Word to echo: "
-                        , code [] [ text w ]
-                        ]
-
-                panelList =
-                    case model.reply of
-                        Nothing ->
-                            ul [ class "list-group" ]
-                                [ li [ class "list-group-item" ]
-                                    [ button
-                                        [ class "btn btn-primary"
-                                        , onClick GetReply
-                                        ]
-                                        [ text "Get the reply" ]
-                                    ]
-                                ]
-
-                        Just reply ->
-                            ul [ class "list-group" ]
-                                [ li
-                                    [ class "list-group-item" ]
-                                    [ text reply ]
-                                ]
+                panelWordList =
+                    Html.map PanelWordList
+                        PanelWordList.View.root
+                        model.panel_word_list
             in
                 div [ class "container" ]
-                    [ div [ class "panel panel-default" ]
-                        [ panelHeader
-                        , panelBody
-                        , panelList
-                        ]
+                    [ panelWordList
+                    , panelWordEcho
                     ]
