c00d17ee7592af9cc84bdfea19d38c88aeac7c53
Robert Hardy <robbert@gmail.com>
2016-11-20 08:41:34 +0000

    Make init and update trivial for the moment.

--- app/frontend/AppMain/State.elm
+++ app/frontend/AppMain/State.elm
@@ -6,23 +6,8 @@ import AppMain.Types exposing (..)
 
 init : ( Model, Cmd Msg )
 init =
-    ( Model (Just "Buongiorno") Nothing, Cmd.none )
+    ( Model (Just "Buongiorno"), Cmd.none )
 
 
 update msg model =
-    case model.word of
-        Nothing ->
-            ( model, Cmd.none )
-
-        Just word ->
-            case msg of
-                GetReply ->
-                    ( model, getReply word )
-
-                Reply reply ->
-                    ( { model | reply = Just reply }
-                    , Cmd.none
-                    )
-
-                otherwise ->
-                    ( model, Cmd.none )
+    ( model, Cmd.none )
