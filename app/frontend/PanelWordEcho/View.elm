c5a3bef187a816552fc80405d04819e1b106735c
Robert Hardy <robbert@gmail.com>
2016-11-20 08:34:44 +0000

    Tidy up the view code in the word echo panel.

--- app/frontend/PanelWordEcho/View.elm
+++ app/frontend/PanelWordEcho/View.elm
@@ -7,21 +7,28 @@ import PanelWordEcho.Types exposing (..)
 
 
 root model =
-    let
-        the_word =
-            case model.word of
-                Nothing ->
-                    "No word supplied"
+    case model.word of
+        Nothing ->
+            text ""
 
-                Just w ->
-                    w
+        Just w ->
+            let
+                panelHeader =
+                    div
+                        [ class "panel-heading" ]
+                        [ div
+                            [ class "panel-title" ]
+                            [ h1 [] [ text "Word echo" ] ]
+                        ]
 
-        reply_section =
-            case model.word of
-                Nothing ->
-                    text ""
+                panelBody =
+                    div
+                        [ class "panel-body" ]
+                        [ text "Word to echo: "
+                        , code [] [ text w ]
+                        ]
 
-                otherwise ->
+                panelList =
                     case model.reply of
                         Nothing ->
                             ul [ class "list-group" ]
@@ -36,33 +43,15 @@ root model =
 
                         Just reply ->
                             ul [ class "list-group" ]
-                                [ li [ class "list-group-item" ] [ text reply ]
+                                [ li
+                                    [ class "list-group-item" ]
+                                    [ text reply ]
                                 ]
-    in
-        div [ class "container" ]
-            [ panel "Word echo" the_word reply_section
-            ]
-
-
-panel title word reply_section =
-    let
-        panelHeading title =
-            div
-                [ class "panel-heading" ]
-                [ div
-                    [ class "panel-title" ]
-                    [ h1 [] [ text title ] ]
-                ]
-
-        panelBody word =
-            div
-                [ class "panel-body" ]
-                [ code [] [ text word ]
-                ]
-    in
-        div
-            [ class "panel panel-default" ]
-            [ panelHeading title
-            , panelBody word
-            , reply_section
-            ]
+            in
+                div [ class "container" ]
+                    [ div [ class "panel panel-default" ]
+                        [ panelHeader
+                        , panelBody
+                        , panelList
+                        ]
+                    ]
