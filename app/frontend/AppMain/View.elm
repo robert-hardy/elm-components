99fbb49a96f16d63c449e46fda91cd616200db89
Robert Hardy <robbert@gmail.com>
2016-11-20 09:03:02 +0000

    Fix imports in view and add second panel.

--- app/frontend/AppMain/View.elm
+++ app/frontend/AppMain/View.elm
@@ -1,9 +1,12 @@
 module AppMain.View exposing (..)
 
 import Html exposing (..)
+import Html.App
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
 import AppMain.Types exposing (..)
+import PanelWordEcho.View
+import PanelWordList.View
 
 
 root model =
@@ -14,9 +17,14 @@ root model =
         Just w ->
             let
                 panelWordList =
-                    Html.map PanelWordList
+                    Html.App.map PanelWordList
                         PanelWordList.View.root
                         model.panel_word_list
+
+                panelWordEcho =
+                    Html.App.map PanelWordEcho
+                        PanelWordEcho.View.root
+                        model.panel_word_echo
             in
                 div [ class "container" ]
                     [ panelWordList
